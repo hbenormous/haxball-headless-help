e59e8aeb0368d97ebf532a8490b25760ea1f4b4b
hbenormous <flavioremigio7@gmail.com>
2021-11-27 23:44:30 -0300

    Update webhook-tutorial.js

--- scripts/webhook-tutorial.js
+++ scripts/webhook-tutorial.js
@@ -2,7 +2,7 @@
 Autor: aazin#8560
 Titulo: Webhook tutorial
 Descrição: Tutorial de como construir um webhook no haxball.
-Data de criação: 12:56PM
+Data de criação: 24/11/2021
 
 Contatos:
     Discord: https://discord.gg/UWYuxwEKfn
@@ -23,17 +23,18 @@ var room = HBInit({
 });
 
 // Globais
-var webhookURL = "URL da webhook" // https://support.discord.com/hc/pt-br/articles/228383668-Usando-Webhooks
-
+// https://support.discord.com/hc/pt-br/articles/228383668-Usando-Webhooks
+var webhookURL = "https://discord.com/api/webhooks/903822016916893758/DYLVgDBfYWpIyFo5RGzuH4fSvGL9n8GbaNs-0RNWIFazjlsT6M7U7383w_3aQ_je08lb"; // Webhook link
+var webhookNOME = '.'; // Webwook nome
 
 // Funções
 function discord(mensagem) {
 var chamar = new XMLHttpRequest();
-    chamar.open("POST",webhookURL);
+    chamar.open("POST",webhookURL); // Webhook Link
     chamar.setRequestHeader('Content-type', 'application/json');
 
     var weebhook_dados = {
-        username: 'nome', // Nome do webhook
+        username: webhookNOME, // Nome do webhook
         content: mensagem // Callback
     };
 
