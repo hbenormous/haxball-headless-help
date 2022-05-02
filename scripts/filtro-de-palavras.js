31d142272f880d10d3d45eda93994c65cf0328cc
hbenormous <flavioremigio7@gmail.com>
2022-05-02 17:39:48 -0300

    Update filtro-de-palavras.js

--- scripts/filtro-de-palavras.js
+++ scripts/filtro-de-palavras.js
@@ -4,7 +4,7 @@ let
 palavras = ["fdp", "porra", "arrombado"],
 regex = new RegExp(palavras.join("|"), 'gi');
 
-room.onPlayerChat = function(player, message) {
+room.onPlayerChat = (player, message) => {
     if (message.match(regex)) {
         room.sendAnnouncement("Sem xingar.", player.id);
         return false;
