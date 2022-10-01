2080b179575198427ec6ad34265a52ba478282ac
hbenormous <flavioremigio7@gmail.com>
2022-09-30 21:44:29 -0300

    discord webhook

--- scripts/webhook-tutorial.js
+++ scripts/webhook-tutorial.js
@@ -1,48 +1,37 @@
-/*
-Autor: aazin#8560
-Titulo: Webhook tutorial
-Descrição: Tutorial de como construir um webhook no haxball.
-Data de criação: 24/11/2021
+/**
+ * TUTORIAL
+ * https://support.discord.com/hc/pt-br/articles/228383668-Usando-Webhooks
+ */
+ const URLs = {
+    chat: "SUA URL DO WEBHOOK"
+};
 
-Contatos:
-    Discord: https://discord.gg/UWYuxwEKfn
-    Site: https://haxballheadlesshelp.ueuo.com/
-    Github: https://github.com/hbenormous/haxball-headless-help
+const room = HBInit({});
 
-Links:
-    Como criar webhook: https://support.discord.com/hc/pt-br/articles/228383668-Usando-Webhooks
-    Haxball Headless Host: https://www.haxball.com/headless
-*/
+room.onPlayerChat = (player, message) => {
 
-// Definindo função HBInit
-var room = HBInit({
-    roomName: 'Haxball Headless Help',
-    maxPlayers: 10,
-    public: true,
-    noPlayer: true
-});
+    sendMessageToDiscord(URLs.chat, {
+        content: `${player.name}: ${message}`
+    });
 
-// Globais
-// https://support.discord.com/hc/pt-br/articles/228383668-Usando-Webhooks
-var webhookURL = 'webhook link';
-var webhookNOME = 'webhook nome';
-
-// Funções
-function discord(mensagem) {
-var chamar = new XMLHttpRequest();
-    chamar.open("POST",webhookURL); // Webhook Link
-    chamar.setRequestHeader('Content-type', 'application/json');
-
-    var weebhook_dados = {
-        username: webhookNOME, // Nome do webhook
-        content: mensagem // Callback
-    };
-
-    chamar.send(JSON.stringify(weebhook_dados));
 }
 
+function sendMessageToDiscord(url, params = {}) {
 
-// on
-room.onPlayerChat = function(player, message) {
-    discord(player.name + ': ' + message); // Exemplo de webhook
+    return new Promise((resolve, reject) => {
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("POST", url);
+
+        xhr.setRequestHeader("Content-Type", "application/json"); // formato json
+
+        /**
+         * DISCORD WEBHOOK PARAMS
+         * https://birdie0.github.io/discord-webhooks-guide/index.html
+         */
+        xhr.send(JSON.stringify(params));
+
+    });
+
 }
